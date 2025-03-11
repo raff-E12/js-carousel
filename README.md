e13730c384f07a7fe0c0c9e9b8bb078196bce7aa
Raff_12 <raffelecoppola381@gmail.com>
2025-03-11 19:50:35 +0100

    feat: Conclusione del progetto

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Js- Slider
+
+Ecco un piccolo progetto fatto da me, è uno slider fatto da me in meniera definitiva, nel caso in futuro aggiornerò il progetto, buona visualizzazione del codice :).
+
+
--- app.js
+++ app.js
@@ -7,12 +7,42 @@
 
 // Nominazione delle seguenti variabili:
 
-let arrow_left = document.getElementById("btn-left");
-let arrow_right = document.getElementById("btn-right");
+const arrow_left = document.getElementById("btn-left");
+const arrow_right = document.getElementById("btn-right");
 let Sliders = document.querySelectorAll(".slide-box");
+let cont_index = 0;
 
 // Funzione di Slider
 
-const HandleFunSlidersMotion = ()=>{
-    
+//Funzione di Movimento di Sinistra
+
+const HandleFunSlidersMotionLeft = ()=>{
+    Sliders[cont_index].classList.add("none");
+    if (cont_index > 0) {
+        cont_index--;
+        // console.log(Sliders[cont_index].getAttribute("id"));
+    } else {
+        cont_index = Sliders.length-1;
+        // console.log("reset")
+    }
+    Sliders[cont_index].classList.remove("none");
 }
+
+//Funzione di Movimento di Destra
+
+const HandleFunSlidersMotionRight = ()=>{
+    Sliders[cont_index].classList.add("none");
+     if (cont_index < Sliders.length-1){
+        // console.log(Sliders[cont_index].getAttribute("id"));
+        cont_index++;
+    } else {
+      cont_index = 0;
+      //console.log("reset")
+    }
+    Sliders[cont_index].classList.remove("none");
+}
+
+window.addEventListener("load", ()=>{
+  arrow_left.addEventListener("click", HandleFunSlidersMotionLeft);
+  arrow_right.addEventListener("click", HandleFunSlidersMotionRight);
+})
