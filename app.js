0a7dd95885073784c45f81bce6081b2689054ce9
Raff_12 <raffelecoppola381@gmail.com>
2025-03-11 17:30:11 +0100

    feat: Pubblicazione delle risorse e proseguimento del progetto

---
+++ app.js
@@ -0,0 +1,18 @@
+// Scopo del Progetto:
+// Realizzazione di un carosello come Vetrina
+// Ecco i punti specifici:
+// - ripassando argomenti sui quali vi sentite più indietro
+// - recuperando esercitazioni
+// - provando ad analizzare il codice del progetto carousel e realizzare la vostra versione.
+
+// Nominazione delle seguenti variabili:
+
+let arrow_left = document.getElementById("btn-left");
+let arrow_right = document.getElementById("btn-right");
+let Sliders = document.querySelectorAll(".slide-box");
+
+// Funzione di Slider
+
+const HandleFunSlidersMotion = ()=>{
+    
+}
